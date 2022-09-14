a752b8eb076559a07bcccedadc95dab79558de23
xavierlin101 <linblue1001@gmail.com>
2022-09-14 19:50:07 +0800

    新增js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const phoneE1 = document.querySelector("#phone-number");
+const analyticsE1 = document.querySelector("#analytics");
+console.log(phoneE1, analyticsE1)
+
+function analytics() {
+    let phoneNumber = phoneE1.value;
+    console.log(phoneNumber);
+}
